c5dbd2e124b610a38103ac6fc7446cc2d8a91fa9
Abhiman Ranaweera <abhiman.max@gmail.com>
2024-06-14 03:20:46 +0530

    style: improve Alert component ui

--- client/src/components/Alert/Alert.tsx
+++ client/src/components/Alert/Alert.tsx
@@ -4,6 +4,6 @@ import styles from './Alert.module.scss';
 const Alert: FC<AlertProps> = ({ skin = "error", onClose, message}) =>
     <div className={`alert-msg ${styles.alert} ${styles[skin]}`}>
 {message}
-<button className={styles.close} onClick={onClose}>Close</button>
+{ onClose && <button className={styles.close} onClick={onClose}>Close</button>}
 </div>
 export default Alert;
